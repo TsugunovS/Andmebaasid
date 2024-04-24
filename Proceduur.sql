c8b36c8bdec3fdffbb660b3d1fafc99d176e215b
Stanislav Tšugunov <120027765+TsugunovS@users.noreply.github.com>
2024-04-24 14:01:22 +0300

    uuendab rezisööri andmed filmiNimi järgi

--- Proceduur.sql
+++ Proceduur.sql
@@ -29,3 +29,11 @@ CREATE PROCEDURE `filmidArv`() NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFIN
 
 -täidab tabeli film
 CREATE PROCEDURE `lisafilm`(IN `uusfilm` VARCHAR(50), IN `kestvus` INT, IN `rezisoor` VARCHAR(50), IN `aasta` INT) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER begin insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values (uusfilm, kestvus, rezisoor, aasta); select * from film; end; 
+
+begin
+insert into film (filmNimetus, kestvus, rezisoor, v_aasta) 
+values (uusfilm, kestvus, rezisoor, aasta);
+select * from film;
+end;
+-uuendab rezisööri andmed filmiNimi järgi
+CREATE PROCEDURE `uuendaRezisorFilmis`(IN `filmnimetus` VARCHAR(50), IN `uusrezisoor` VARCHAR(50)) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER BEGIN SELECT * FROM film WHERE filmNimetus=filmNimetus; UPDATE film SET rezisoor=uusrezisoor WHERE filmNimetus=filmNimetus; SELECT * FROM film WHERE filmNimetus=filmNimetus; END;
