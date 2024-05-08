0be499acafe7394c1f4fad826360c3a1c0b0775b
Stanislav Tsugunov <stanislav.tsugunov@gmail.com>
2024-05-08 12:25:19 +0300

    lisab piirangu, et vaartus suurem kui 0 vaiksem kuni 150

--- sqlLaused.sql
+++ sqlLaused.sql
@@ -85,7 +85,7 @@ update Person
 set Age = 149
 where Id = 8
 
---?
+--lisab piirangu, et väärtus suurem kui 0 väiksem kuni 150
 alter table Person
 add constraint CK_Person_Age check (Age > 0 and Age < 150)
 
