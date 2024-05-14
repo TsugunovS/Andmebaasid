b91d18b1ab3b943758122a7d3cb73f79f328731a
Stanislav Tsugunov <stanislav.tsugunov@gmail.com>
2024-05-14 11:56:09 +0300

    Seejärel loo uus päästik

--- Logitabel.sql
+++ Logitabel.sql
@@ -9,10 +9,35 @@ Create table autoregistr(
 lid int PRIMARY KEY identity(1,1),
 aeg DATETIME,
 toiming  varchar(100),
+numbrimark varchar(20),
+varv varchar(20),
 andmed TEXT,
 kasutaja varchar(100)
 )
 
 INSERT INTO logitabel(autonimi, omanikud)
-values ('Toyota', 4);
+values ('Kia', 2);
 select * from logitabel;
+
+--
+CREATE TRIGGER AutoLisamine
+ON logitabel
+FOR INSERT
+AS
+BEGIN
+	INSERT INTO autoregistr(kasutaja, aeg, numbrimark, varv, toiming, andmed)
+	SELECT USER,
+			GETDATE(),
+			'548 JKF',
+			'Must',
+			'Auto on lisatud',
+			inserted.autonimi
+	FROM inserted;
+END
+
+INSERT INTO logitabel(autonimi, omanikud)
+values ('BMW', 7);
+select * from autoregistr;
+select * from logitabel;
+
+
